e6cf57915f54b77b4b5a3ed52444ef94e242aa5a
Thiago Magalhães <thiagomagalhaes2004@hotmail.com>
2022-01-29 21:26:18 -0300

    hotfix routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   mount Sidekiq::Web => '/sidekiq'
 
-  root to: 'pages#index'
+  root to: 'pages#home'
   resources :campaigns, except: [:new] do
     post 'rafle', on: :member # Exemplo: /campaigns/:id/rafle
     # post 'rafle', on: :collection # Exemplo: /campaigns/rafle
